ee442e6050617556d1b072da63d1998c9e0d291b
Darshanhs022 <darshanhs2525@gmail.com>
2026-01-05 10:02:04 +0530

    git push :q! :q! clear :wq

--- SQL/08_baseline_progress.sql
+++ SQL/08_baseline_progress.sql
@@ -4,6 +4,7 @@ CREATE TABLE baseline_progress (
     floor_level INTEGER NOT NULL,
     activity_name TEXT NOT NULL,
     completed_units INTEGER NOT NULL CHECK (completed_units >= 0),
+    baseline_date DATE NOT NULL,
     CONSTRAINT pk_baseline PRIMARY KEY (project_code, tower_id, floor_level, activity_name),
     CONSTRAINT fk_baseline_project
         FOREIGN KEY (project_code)
--- SQL/09_raw_progress_input.sql
+++ SQL/09_raw_progress_input.sql
@@ -1,10 +1,11 @@
 CREATE TABLE raw_progress_input (
     raw_id SERIAL PRIMARY KEY,
     project_code TEXT NOT NULL,
-    tower_name TEXT NOT NULL,
+    tower_id INTEGER NOT NULL
     floor_level INTEGER NOT NULL,
     activity_name TEXT NOT NULL,
     completed_units INTEGER NOT NULL CHECK (completed_units >= 0),
     entry_date DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (tower_id) REFERENCES towers (tower_id)
 );
